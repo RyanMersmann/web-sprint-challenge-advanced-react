f3d2fb457007d3f384d406a1f0415701e003c1d6
RyanMersmann <mersmannryan@gmail.com>
2020-06-13 11:17:46 -0600

    added testing for checkout form

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,40 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
+import App from "../App"
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    const app = render(<App />);
+    const header = app.getByText(/react plants/i)
+    expect(header).toBeInTheDocument();
+});
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", () => {
+    const { getByRole, getByLabelText, findAllByText } = render(<CheckoutForm />);
+
+    fireEvent.change(getByLabelText(/first name/i), {
+        target: { value: "Ryan" }
+      });
+      fireEvent.change(getByLabelText(/last name/i), {
+        target: { value: "Mersmann" }
+      });
+      fireEvent.change(getByLabelText(/address/i), {
+        target: { value: "5555 Village Road" }
+      });
+      fireEvent.change(getByLabelText(/city/i), {
+        target: { value: "Orem" }
+      });
+      fireEvent.change(getByLabelText(/state/i), {
+        target: { value: "Utah" }
+      });
+      fireEvent.change(getByLabelText(/zip/i), {
+        target: { value: "84057" }
+      });
+
+    const button = getByRole('button')
+    fireEvent.click(button)
+    const formData = findAllByText(/ryan/i)
+    expect(formData).toBeInTheDocument
+});
--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,13 +1,11 @@
 // write your custom hook here to control your checkout form
 import { useState } from 'react';
 
-export const useForm = () => {
+export const useForm = (initialValue) => {
     const [values, setValues] = useState(initialValue);
 
-    const handleChanges = () => {
-        setValues({
-            ...values, [e.target.name]: e.target.value
-        })
+    const handleChanges = (e) => {
+        setValues({ ...values, [e.target.name]: e.target.value })
     };
     return [values, setValues, handleChanges]
 };
